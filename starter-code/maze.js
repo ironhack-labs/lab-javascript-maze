805698ed7b2b0062155fd8532f6a4eb910124a30
ahuertam <ahuertam@yahoo.es>
2017-01-26 16:27:27 +0100

    Not done

--- starter-code/maze.js
+++ starter-code/maze.js
@@ -8,23 +8,46 @@ var Maze = function(map, exit) {
   this._exit = {
     row: exit.row,
     col: exit.col
-  }
-}
+  };
+};
 
 Maze.prototype.turnLeft      = function(){
-}
+  this._miner.dir = this._miner.dir === 0 ? 3 : this._miner.dir -= 1;
+};
 
 Maze.prototype.turnRight     = function(){
-}
+  this._miner.dir = this._miner.dir === 3 ? 0 : this._miner.dir += 1;
+};
+
 
 Maze.prototype.isPathForward = function(){
-}
+  switch (this._miner.dir) {
+    case 0:
+      if ((maze._miner.row+1) > 6){
+        return false;}
+      else{  return this.map[maze._miner.row +1][this.col]; }
+      break;
+    case 1:
+      return false;
+    case 2:
+      if ((maze._miner.row-1) < 0 ){
+        return false; }
+        else{ return this.map[maze._miner.row -1][this.col]; }
+      break;
+    case 3:
+      return false;
+    default:
+      return false;
+  }
+};
 
 Maze.prototype.isPathLeft    = function(){
-}
 
+};
 Maze.prototype.isPathRight   = function(){
-}
+  //if (this._miner.dir === this._miner.dir+1){return true;}
+  //else{return false;}
+};
 
 Maze.prototype.moveForward   = function(){
 }
