d3d3a90a6c7a67df9ed378972565a8f1b7cd1e05
Manuel Lintz <manuel.lintz@gmail.com>
2017-01-27 08:55:27 +0100

    done

--- starter-code/maze.js
+++ starter-code/maze.js
@@ -8,20 +8,106 @@ var Maze = function(map, exit) {
   this._exit = {
     row: exit.row,
     col: exit.col
-  }
-}
+  };
+};
 
 Maze.prototype.turnLeft      = function(){
-}
+   this._miner.dir = this._miner.dir === 0 ? 3 : this._miner.dir -= 1;
+
+};
 
 Maze.prototype.turnRight     = function(){
-}
+  this._miner.dir = this._miner.dir === 3 ? 0 : this._miner.dir += 1;
+};
 
+///////////////////////////////////
+Maze.prototype.oneRowUp = function(){
+  var x = this._miner.row;
+  var y = this._miner.col;
+  return [x-1,y];
+  };
+
+Maze.prototype.oneRowDown = function(){
+  var x = this._miner.row;
+  var y = this._miner.col;
+  return [x+1,y];
+};
+
+Maze.prototype.oneColRight = function(){
+  var x = this._miner.row;
+  var y = this._miner.col;
+  return [x,y+1];};
+
+Maze.prototype.oneColLeft = function(){
+  var x = this._miner.row;
+  var y = this._miner.col;
+  return [x,y-1];
+};
 Maze.prototype.isPathForward = function(){
-}
+  var x = this._miner.row;
+  var y = this._miner.col;
+
+  switch(this._miner.dir){
+    case 0:
+        if(x-1 >=0 && this._maze[x-1][y]===true){
+              return true;
+            }
+        else {
+            return false;
+          }
+      break;
+
+   case 1:
+       if(y+1<=5 && this._maze[x][y+1]===true){
+            return true;
+
+       }else{
+         return false;
+       }
+
+      break;
+
+//if direction is down
+    case 2:
+      if(x+1<=6 && this._maze[x+1][y]===true)
+       { return true;}
+      else{
+        return false;}
+      break;
+
+    case 3:
+    if(y-1>=0 && this._maze[x][y-1]===true){
+         return true;}
+      else{
+        return false;
+      }
+
+    break;
+
+  }
+};
 
 Maze.prototype.isPathLeft    = function(){
-}
+  switch (this._miner.dir){
+    case 0:
+     var c = this.oneColLeft();
+     var x= c[0];
+     var y= c[1];
+     return (x>=0 && this._maze[x][y])? true : false;
+
+     case 1:
+      var c = this.oneRowUp();
+      var x= c[0];
+      var y= c[1];
+      return (y>=0 && this._maze[x][y])? true : false;
+
+
+
+
+
+
+  }
+};
 
 Maze.prototype.isPathRight   = function(){
 }
