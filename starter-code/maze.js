65587f2d6f9903aa1886e6e93c4c2e4e3e6a206a
TuckerDS <jjperez.tucker.dev@gmail.com>
2017-04-13 16:13:36 +0200

    version 1

--- starter-code/maze.js
+++ starter-code/maze.js
@@ -11,38 +11,67 @@ var Maze = function(map, exit) {
   };
 };
 
-Maze.prototype.turnLeft      = function(dir){
-  switch(dir) {
+Maze.prototype.turnLeft = function(){
+
+  switch(this._miner.dir) {
     case 0:
-      return ;
+      this._miner.dir = 3;
+      break;
     case 1:
-      return 0;
+      this._miner.dir = 0;
+      break;
     case 2:
-      return 1;
+      this._miner.dir = 1;
+      break;
     case 3:
-      return 2;
+      this._miner.dir = 2;
+      break;
     }
 };
 
-Maze.prototype.turnRight     = function(dir){
-  switch(dir) {
+Maze.prototype.turnRight = function(){
+  switch(this._miner.dir) {
     case 0:
-      return 1;
+      this._miner.dir= 1;
+      break;
     case 1:
-      return 2;
+      this._miner.dir= 2;
+      break;
     case 2:
-      return 3;
+      this._miner.dir= 3;
+      break;
     case 3:
-      return 0;
+      this._miner.dir= 0;
+      break;
   }
 };
 
 Maze.prototype.isPathForward = function(){
-  if(this._miner.dir === 2){
-    return true;
-  }else{
+/*if (this._miner.dir === 2){
+  return true;
+}else {
+  return true;
+}*/
+
+switch(this._miner.dir) {
+  case 0:
     return false;
-  }
+  case 1:
+    return false;
+  case 2:
+    return true;
+  case 3:
+    return false;
+  default:
+    map[maze._miner.row][maze._miner.col];
+}
+
+/*
+  if (map[maze._miner.row][maze._miner.col]== "T") {
+    return true;
+  }else {
+    return false;
+  }*/
 };
 
 Maze.prototype.isPathLeft    = function(){
