f2134b2862b42edba8ce7f90fac588713980dfc4
Fcozar <franciscodecozar@gmail.com>
2017-04-14 09:43:28 +0200

    Madrid Fran y Fernando

--- starter-code/maze.js
+++ starter-code/maze.js
@@ -8,16 +8,20 @@ var Maze = function(map, exit) {
   this._exit = {
     row: exit.row,
     col: exit.col
-  }
-}
+  };
+};
 
-Maze.prototype.turnLeft      = function(){
-}
+Maze.prototype.turnLeft = function(){
+//  this._maze.dir = this._maze.dir === 0 ? this._maze.dir = 3 : this._maze.dir -= 1;
+  this._miner.dir = (this._miner.dir + 3) % 4;
+};
 
-Maze.prototype.turnRight     = function(){
-}
+Maze.prototype.turnRight = function(){
+  this._miner.dir = (this._miner.dir +1) % 4;
+};
 
 Maze.prototype.isPathForward = function(){
+  
 }
 
 Maze.prototype.isPathLeft    = function(){
