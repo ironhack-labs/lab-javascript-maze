ce000ec9de2c154f9a32f95108a4f107776277fd
ByronGBP <byrongbp@gmail.com>
2017-08-24 18:56:02 +0200

    done

--- starter-code/test/maze-test.js
+++ starter-code/test/maze-test.js
@@ -99,7 +99,7 @@ describe('Maze', function() {
     });
 
     describe('check up', function(){
-      beforeEach(function() { maze._miner.dir = 0 });
+      beforeEach(function() { maze._miner.dir = 0; });
       it('first row', function() {
         expect(maze.isPathForward()).to.equal(false);
       });
@@ -116,7 +116,7 @@ describe('Maze', function() {
     });
 
     describe('check down', function(){
-      beforeEach(function() { maze._miner.dir = 2 });
+      beforeEach(function() { maze._miner.dir = 2; });
       it('last row', function() {
         maze._miner.row = 6;
         maze._miner.col = 4;
